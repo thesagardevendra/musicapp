57b638fc31c34de3214378320ca1523623d73653
thesagardevendra <78974018+thesagardevendra@users.noreply.github.com>
2021-10-13 12:00:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+// const playBtn=document.querySelectorAll(".main-cards");
+
+// const hoverBtn=document.getElementsByClassName("play-button");
+
+
+// for(let i=0;i<playBtn.length;i++){
+    
+//     playBtn[i].addEventListener("mouseover",(e)=>{
+//         playBtn[i].style.backgroundColor="#3b3b3b";
+//         hoverBtn.style.zIndex="2";
+//         hoverBtn.style.dsiplay="block";
+
+//         });   
+//         playBtn[i].addEventListener("mouseout",(e)=>{
+//             playBtn[i].style.backgroundColor="#333333";
+//             });
+// }
+
